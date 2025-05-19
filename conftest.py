147d101aa83af704849e2e43d9fd91a591543082
Magdalena Niemiec <magdalena@Magdalenas-MacBook-Pro.local>
2025-05-19 11:56:38 +0200

    Fix webdriver.Chrome usage: pass Service object instead of string

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     driver.maximize_window()
     yield driver
     driver.quit()
-
