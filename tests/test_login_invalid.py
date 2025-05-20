f1d46f55f513e32c2a3ddbf85213da09b1ff5fcb
Magdalena Niemiec <magdalena@Magdalenas-MacBook-Pro.local>
2025-05-20 08:54:13 +0200

    Add parameterized test for invalid login for each specific error message

--- tests/test_login_invalid.py
+++ tests/test_login_invalid.py
@@ -1,14 +1,14 @@
 from pages.login_page import LoginPage
 import pytest
 
-@pytest.mark.parametrize("username,password", [
-    ("wrong_user", "secret_sauce"),
-    ("standard_user", "wrong_pass"),
-    ("", "secret_sauce"),
-    ("standard_user", ""),
-    ("", "")
+@pytest.mark.parametrize("username,password,expected_error", [
+    ("wrong_user", "secret_sauce", "Username and password do not match any user"),
+    ("standard_user", "wrong_pass", "Username and password do not match any user"),
+    ("", "secret_sauce", "Username is required"),
+    ("standard_user", "", "Password is required"),
+    ("", "", "Username is required")
 ])
-def test_invalid_login(browser, username, password):
+def test_invalid_login(browser, username, password, expected_error):
 
     page = LoginPage(browser)
     page.load()
@@ -16,5 +16,5 @@ def test_invalid_login(browser, username, password):
     page.login(username, password)
 
     error_text = page.get_error_message()
-    assert error_text != ""
+    assert expected_error.lower() in error_text.lower()
     
